2067060e82de3c4de0c2ee3394a1ff2c653dd1da
Muhammed EMEN <emiremen001@gmail.com>
2022-09-09 18:19:15 +0300

    Initial Commit

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    const float minPathUpdateTime = .2f;
+    const float pathUpdateMoveThresold = .5f;
+
+    public Transform target;
+    public float speed = 10;
+    public float turnSpeed = 3;
+    public float turnDistance = 5;
+    public int stoppingDistance = 10;
+
+    Path path;
+
+    private void Start()
+    {
+        StartCoroutine(UpdatePath());
+    }
+
+    public void OnPathFound(Vector3[] waypoints, bool pathSuccessful)
+    {
+        if (pathSuccessful)
+        {
+            path = new Path(waypoints, transform.position, turnDistance, stoppingDistance);
+            StopCoroutine("FollowPath");
+            StartCoroutine("FollowPath");
+        }
+    }
+
+    IEnumerator UpdatePath()
+    {
+        if (Time.timeSinceLevelLoad < 3)
+        {
+            yield return new WaitForSeconds(.3f);
+        }
+        PathRequestManager.RequestPath( new PathRequest(transform.position, target.position, OnPathFound));
+
+        float squareMoveThresold = pathUpdateMoveThresold * pathUpdateMoveThresold;
+        Vector3 targetPosOld = target.position;
+
+        while (true)
+        {
+            yield return new WaitForSeconds(minPathUpdateTime);
+            if ((target.position - targetPosOld).sqrMagnitude > squareMoveThresold)
+            {
+                PathRequestManager.RequestPath(new PathRequest(transform.position, target.position, OnPathFound));
+                targetPosOld = target.position;
+            }
+        }
+    }
+
+    IEnumerator FollowPath()
+    {
+        bool followingPath = true;
+        int pathIndex = 0;
+        transform.LookAt(path.lookPoints[0]);
+
+        float speedPercent = 1;
+
+
+        while (followingPath)
+        {
+            Vector2 pos2D = new Vector2(transform.position.x, transform.position.z);
+            while (path.turnBoundaries[pathIndex].HasCrossedLine(pos2D))
+            {
+                if (pathIndex == path.finishLineIndex)
+                {
+                    followingPath = false;
+                    break;
+                }
+                else
+                {
+                    pathIndex++;
+                }
+            }
+
+            if (followingPath)
+            {
+                if (pathIndex >= path.slowDownIndex && stoppingDistance > 0)
+                {
+                    speedPercent = Mathf.Clamp01(path.turnBoundaries[path.finishLineIndex].DistanceFromPoint(pos2D) / stoppingDistance);
+                    if (speedPercent < 0.01f)
+                    {
+                        followingPath = false;
+                    }
+                }
+
+                Quaternion targetRotation = Quaternion.LookRotation(path.lookPoints[pathIndex] - transform.position);
+                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
+                transform.Translate(Vector3.forward * Time.deltaTime * speed * speedPercent, Space.Self);
+            }
+
+            yield return null;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (path != null)
+        {
+            path.DrawWithGizmos();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# A-Star-Pathfinding
+# A-Star-Pathfinding-With-Unity
 Sample of A* Pathfinding with Unity.
+
+![screenshots](https://github.com/emiremen/A-Star-Pathfinding-With-Unity/blob/main/Screenshots/SS1.gif)
+
+![screenshots](https://github.com/emiremen/A-Star-Pathfinding-With-Unity/blob/main/Screenshots/SS2.gif)
